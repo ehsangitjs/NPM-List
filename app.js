b8ab57a10cc1d1ed8966166760922003fcb44448
Ehsan <72614608+ehsangitjs@users.noreply.github.com>
2021-11-07 19:25:27 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const shell = require('shelljs')
+var name = process.env.USERNAME
 var all = shell.ls(`C:\\Users\\${name}\\AppData\\Roaming\\npm\\node_modules`).stdout.split('\n')
 all.pop()
 all.forEach((element)=>{
